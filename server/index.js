732459e0479e3ce3218e1ca044ef999e9afc6e4d
Anshik Suhane <anshiksuhane9@gmail.com>
2026-03-13 01:17:34 +0530

    Update CORS origin for production server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://prepgenius-ai-server-i6ia.onrender.com/",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
